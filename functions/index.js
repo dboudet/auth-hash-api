e40b2263e462606d285feab91a5276f9600088f5
Dan Boudet <dboudet04@gmail.com>
2021-08-18 12:13:11 -0400

    initial commit - authorization and authentication. login, signup and profile routes set up

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,15 @@
 const functions = require("firebase-functions")
 const express = require("express")
 const cors = require("cors")
-const { userSignup, userLogin } = require("./src/users")
+const { userSignup, userLogin, updateUser } = require("./src/users")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.post("/signup", userSignup)
 app.post("/login", userLogin)
+app.post("/signup", userSignup)
+
+app.patch("/profile", updateUser)
 
 exports.app = functions.https.onRequest(app)
--- functions/src/users.js
+++ functions/src/users.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin")
+const jwt = require("jsonwebtoken")
 const creds = require("../credentials.json")
+const { secret } = require("../secrets")
 
 function connectDb() {
   if (!admin.apps.length) {
@@ -11,7 +13,6 @@ function connectDb() {
 }
 
 exports.userSignup = (req, res) => {
-  // check if valid request (email & pw present in request)
   if (!req.body || !req.body.email || !req.body.password) {
     res.status(400).send({
       message: "Invalid request: missing email and/or password",
@@ -20,17 +21,17 @@ exports.userSignup = (req, res) => {
     })
     return
   }
-  // if so, connect to db
   const db = connectDb()
-  // insert into db (assigns email as doc id) and return success
   db.collection("users")
-    .doc(req.body.email.toLowerCase())
-    .set(req.body)
+    // .doc(req.body.email.toLowerCase())
+    .add(req.body)
     .then(() => {
+      const token = jwt.sign({ email: req.body.email }, secret)
       res.send({
         message: "User created successfully",
         status: 200,
         success: true,
+        token,
       })
     })
     .catch((err) => {
@@ -43,5 +44,86 @@ exports.userSignup = (req, res) => {
 }
 
 exports.userLogin = (req, res) => {
-  res.send("ok")
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(400).send({
+      message: "Invalid request: missing email and/or password",
+      status: 400,
+      success: false,
+    })
+    return
+  }
+
+  const db = connectDb()
+
+  db.collection("users")
+    .where("email", "==", req.body.email.toLowerCase())
+    .where("password", "==", req.body.password)
+    .get()
+    .then((userCollection) => {
+      if (userCollection.docs.length) {
+        let user = userCollection.docs[0].data()
+        user.password = undefined
+        const token = jwt.sign(user, secret)
+        res.send({
+          message: "User logged in successfully",
+          status: 200,
+          success: true,
+          token,
+        })
+      } else {
+        res.status(401).send({
+          message: "Invalid email or password",
+          status: 401,
+          success: false,
+        })
+      }
+    })
+    .catch((err) =>
+      res.status(500).send({
+        message: "Error:" + err.message,
+        status: 500,
+        success: false,
+      })
+    )
+}
+
+exports.updateUser = (req, res) => {
+  // decode token and validate
+  const bearer = req.headers["authorization"]
+  if (!bearer) {
+    res.status(403).send({
+      success: false,
+      status: 403,
+      message: "Access denied: no token provided",
+    })
+  }
+  const token = bearer.split(" ")[1]
+  const decoded = jwt.verify(token, secret)
+  console.log(decoded)
+  const db = connectDb()
+  db.collection("users")
+    .where("email", "==", decoded.email)
+    .get()
+    .then((collection) => {
+      const userId = collection.docs[0].id
+      db.collection("users")
+        .doc(userId)
+        .update(req.body)
+        .then((docRef) => {
+          res.send({
+            success: true,
+            status: 202,
+            message: "User updated successfully",
+          })
+        })
+    })
+    .catch((err) => {
+      res.status(500).send({
+        status: 500,
+        success: false,
+        message: "Server error: Failed to update user.",
+      })
+    })
+  //check payload
+  //udpate user with payload
 }
